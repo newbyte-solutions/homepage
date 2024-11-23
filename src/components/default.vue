1481feef5e588ede2589631eec3d626631e227a1
ARPCharlie <charlie@arp-lan.com>
2024-11-23 23:05:18 +0100

    Chore: spacing and cleanup

--- src/components/default.vue
+++ src/components/default.vue
@@ -23,11 +23,8 @@
       </div>
   
       <!-- Mobile Menu -->
-      <div
-        v-if="menuOpen"
-        class="fixed top-0 left-0 w-screen h-screen bg-white flex flex-col items-center justify-center space-y-6 z-40"
-      >
-      <span class="font-bold text-xl absolute top-6 left-4">Newbyte</span>
+      <div v-show="menuOpen" class="fixed top-0 left-0 w-screen h-screen bg-white flex flex-col items-center justify-center space-y-6 z-40 transform transition-transform duration-300" :class="{ 'translate-y-0': menuOpen, '-translate-y-full': !menuOpen }">
+        <span class="font-bold text-xl absolute top-6 left-4">Newbyte</span>
         <button @click="toggleMenu" class="absolute top-0 right-4 text-gray-700 focus:outline-none">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
